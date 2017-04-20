f1d629f7548046ac78047db915d4eee656869570
smriti <smriti@smriti-pc.fios-router.home>
2017-04-20 10:39:29 -0400

    give any message or your sign
    Signed-off-by: priya

---
+++ src/test/java/com/learnautomationmaven/demomaven.java
@@ -0,0 +1,11 @@
+package com.learnautomationmaven;
+
+import org.testng.annotations.Test;
+
+public class demomaven {
+@Test
+	public void test()
+	{
+		System.out.println("Welcome to Maven world");
+	}
+}
